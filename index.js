56b89e364ebbae212a9897ac3ba54c126e0fa33f
Bowser65 <contact.bowser65@gmail.com>
2019-04-19 15:17:29 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Plugin } = require('powercord/entities');
+const { Spinner } = require('powercord/components');
+const { inject, uninject } = require('powercord/injector');
+const { React, Flux, getModule, getModuleByDisplayName } = require('powercord/webpack');
+
+module.exports = class ChannelTyping extends Plugin {
+  async startPlugin () {
+    const TextChannel = await getModuleByDisplayName('ChannelItem');
+    const typingStore = await getModule([ 'getTypingUsers' ]);
+    const currentUser = await getModule([ 'getCurrentUser' ]);
+
+    const TypingIndicator = Flux.connectStores(
+      [ typingStore ],
+      ({ id }) => ({ typingCount: Object.keys(typingStore.getTypingUsers(id)).filter(id => id !== currentUser.getCurrentUser().id).length })
+    )(this._renderTypingElement);
+
+    inject('channeltyping-channel', TextChannel.prototype, 'render', function (args, res) {
+      if (!this.props.selected && !this.props.muted) {
+        res.props.children[1].props.children[2].props.children.push(React.createElement(TypingIndicator, { id: this.props.channel.id }));
+      }
+      return res;
+    });
+  }
+
+  pluginWillUnload () {
+    uninject('channeltyping-channel');
+  }
+
+  _renderTypingElement ({ typingCount }) {
+    return typingCount < 1
+      ? null
+      : React.createElement(Spinner, {
+        type: 'pulsingEllipsis',
+        style: {
+          marginLeft: 5,
+          opacity: 0.7
+        }
+      });
+  }
+};
