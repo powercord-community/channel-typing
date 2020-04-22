fe690576e8860c4baa83e81a3cc10b8f37dd3e67
Bowser65 <bowoser@weeb.services>
2020-04-22 03:44:55 +0200

    License change
    OSL is like AGPL but without the linking bullshit
    Fuck stallman

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-/**
- * Powercord plugin that allows you to see in which channels people are typing
- * Copyright (C) 2019-2020 Bowser65
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/*
+ * Copyright (c) 2020 Bowser65
+ * Licensed under the Open Software License version 3.0
  */
 
 const { Plugin } = require('powercord/entities');
