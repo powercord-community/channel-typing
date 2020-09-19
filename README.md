9e304158de615dc561cbaf5893403997cb34f22c
Bowser65 <bowoser@weeb.services>
2020-09-19 18:42:21 +0200

    Add indicator in DMs

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 > Based on this idea: https://www.reddit.com/r/discordapp/comments/bex98j
 
-Powercord plugin to add a typing indicator on channels where people are typing
+Powercord plugin that shows a typing indicator next to channels where people are typing, even if you're not looking at
+the channel. Also shows if there's someone typing in group DMs and who, even if you don't have it focused.
--- index.js
+++ index.js
@@ -8,37 +8,49 @@ const { Tooltip, Spinner } = require('powercord/components');
 const { inject, uninject } = require('powercord/injector');
 const { React, Flux, getModule, getModuleByDisplayName } = require('powercord/webpack');
 
+const fluxConnector = Flux.connectStoresAsync(
+  [
+    getModule([ 'getTypingUsers' ]),
+    getModule([ 'getCurrentUser' ]),
+    getModule([ 'getMember' ]),
+    getModule([ 'isBlocked', 'isFriend' ])
+  ],
+  ([ typingStore, userStore, memberStore, blockedStore ], { channel }) => ({
+    typing: Object.keys(typingStore.getTypingUsers(channel.id))
+      .filter(id => id !== userStore.getCurrentUser().id && !blockedStore.isBlocked(id))
+      .map(id => {
+        const member = memberStore.getMember(channel.guild_id, id);
+        if (member && member.nick) {
+          return member.nick;
+        }
+        const user = userStore.getUser(id);
+        return user ? user.username : null;
+      })
+      .filter(Boolean)
+  })
+);
+
 module.exports = class ChannelTyping extends Plugin {
   async startPlugin () {
     this.Messages = (await getModule([ 'Messages' ])).Messages;
     const TextChannel = await getModuleByDisplayName('ChannelItem');
-    const blockedStore = await getModule([ 'isBlocked', 'isFriend' ]);
-    const userStore = await getModule([ 'getCurrentUser' ]);
-    const memberStore = await getModule([ 'getMember' ]);
-    const typingStore = await getModule([ 'getTypingUsers' ]);
-    const currentUser = await getModule([ 'getCurrentUser' ]);
+    const PrivateChannel = await getModuleByDisplayName('PrivateChannel');
 
-    const TypingIndicator = Flux.connectStores(
-      [ typingStore, currentUser, userStore, memberStore, blockedStore ],
-      ({ channel }) => ({
-        typing: Object.keys(typingStore.getTypingUsers(channel.id))
-          .filter(id => id !== currentUser.getCurrentUser().id && !blockedStore.isBlocked(id))
-          .map(id => {
-            const member = memberStore.getMember(channel.guild_id, id);
-            if (member && member.nick) {
-              return member.nick;
-            }
-            const user = userStore.getUser(id);
-            return user ? user.username : null;
-          })
-          .filter(Boolean)
-      })
-    )(this._renderTypingElement0.bind(this));
+    const TypingIndicator = fluxConnector((props) => this._renderTypingElement(props));
+    const TypingOrChildren = fluxConnector((props) => this._renderGroupTyping(props));
 
-    inject('channeltyping-channel', TextChannel.prototype, 'renderIcons', function (args, res) {
+    inject('channeltyping-channel', TextChannel.prototype, 'renderIcons', function (_, res) {
       // Other plugins cause this to rerender, leading to duplicated elements.
       if (!this.props.selected && !this.props.muted && !res.props.children.find(c => c && c.type === TypingIndicator)) {
         res.props.children.push(React.createElement(TypingIndicator, { channel: this.props.channel }));
+      }
+      return res;
+    });
+
+    inject('channeltyping-dm-groups', PrivateChannel.prototype, 'renderSubtitle', function (_, res) {
+      if (this.props.channel.isGroupDM()) {
+        const { children } = res.props;
+        res.props.children = React.createElement(TypingOrChildren, { channel: this.props.channel }, children);
       }
       return res;
     });
@@ -46,11 +58,13 @@ module.exports = class ChannelTyping extends Plugin {
 
   pluginWillUnload () {
     uninject('channeltyping-channel');
+    uninject('channeltyping-dm-groups');
   }
 
-  // thats made so plugins can inject custom logic in _renderTypingElement. used by betterfriends.
-  _renderTypingElement0 (props) {
-    return this._renderTypingElement(props);
+  _renderGroupTyping ({ typing, children }) {
+    return typing.length < 1
+      ? children
+      : this._formatTyping(typing);
   }
 
   _renderTypingElement ({ typing }) {
@@ -59,20 +73,7 @@ module.exports = class ChannelTyping extends Plugin {
       : React.createElement(Tooltip,
         {
           position: 'top',
-          text: (typing.length === 1
-            ? this.Messages.ONE_USER_TYPING.format({ a: typing[0] })
-            : typing.length === 2
-              ? this.Messages.TWO_USERS_TYPING.format({
-                a: typing[0],
-                b: typing[1]
-              })
-              : typing.length === 3
-                ? this.Messages.THREE_USERS_TYPING.format({
-                  a: typing[0],
-                  b: typing[1],
-                  c: typing[2]
-                })
-                : this.Messages.SEVERAL_USERS_TYPING)
+          text: this._formatTyping(typing)
         },
         React.createElement('div',
           {
@@ -91,5 +92,22 @@ module.exports = class ChannelTyping extends Plugin {
           })
         )
       );
+  }
+
+  _formatTyping (typing) {
+    return typing.length === 1
+      ? this.Messages.ONE_USER_TYPING.format({ a: typing[0] })
+      : typing.length === 2
+        ? this.Messages.TWO_USERS_TYPING.format({
+          a: typing[0],
+          b: typing[1]
+        })
+        : typing.length === 3
+          ? this.Messages.THREE_USERS_TYPING.format({
+            a: typing[0],
+            b: typing[1],
+            c: typing[2]
+          })
+          : this.Messages.SEVERAL_USERS_TYPING;
   }
 };
